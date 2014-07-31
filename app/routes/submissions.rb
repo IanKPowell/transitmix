efb61ed5f672525eb8a24a491f1b2954046b340e
Danny Whalen <daniel.r.whalen@gmail.com>
2014-07-30 22:49:35 -0400

    Add submission route to email SFMTA

---
+++ app/routes/submissions.rb
@@ -0,0 +1,36 @@
+require 'pony'
+
+module Transitmix
+  module Routes
+    class Submissions < Grape::API
+      Pony.options = {
+        from: 'transitmix-team@codeforamerica.org',
+        to: 'danny+changethis@codeforamerica.org',
+        subject: 'LRV Feedback: A new map has been submitted',
+        via: :smtp,
+        via_options: {
+          enable_starttls_auto: true,
+          authentication: :plain,
+          address:        'smtp.sendgrid.net',
+          port:           587,
+          domain:         'heroku.com',
+          user_name:      ENV['SENDGRID_USERNAME'],
+          password:       ENV['SENDGRID_PASSWORD'],
+        }
+      }
+
+      post '/api/maps/:id/submit' do
+        map = Map.first!(id: params[:id]) # ensure the map exists
+
+        Pony.mail(body: <<-BODY.dedent)
+          Hello,
+
+          A new map has been submitted:
+          http://sfmta.transitmix.net/map/#{map.id}
+
+          - The Transitmix Team
+        BODY
+      end
+    end
+  end
+end
--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ use Rack::Deflater
 run Rack::Cascade.new [
   Transitmix::Routes::Status,
   Transitmix::Routes::Lines,
+  Transitmix::Routes::Submissions,
   Transitmix::Routes::Maps,
   Transitmix::Routes::Home
 ]
